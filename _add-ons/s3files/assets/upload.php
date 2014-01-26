393ecaa053dfdd67dead4f7e7a5f614eb9a4965d
Chad Clark <chad@Chads-iMac.local>
2014-01-26 15:58:40 -0500

    Okay

--- _add-ons/s3files/assets/upload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once '_add-ons/s3files/hooks.s3files.php';
-
-ob_start();
-header('Content-Type: application/json');
-$instance = new Hooks_s3files();
-$instance->uploadFile();
-ob_flush();
