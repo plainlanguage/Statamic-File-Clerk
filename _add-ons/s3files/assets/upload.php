16f2cb3d07e7c457dc7222273540ae7b40a85cf0
Chad Clark <chad@Chads-iMac.local>
2014-01-26 13:45:51 -0500

    Awww yeah

--- _add-ons/s3files/assets/upload.php
+++ _add-ons/s3files/assets/upload.php
@@ -1,9 +1,9 @@
 <?php
 
-require 'uploadHandler.php';
+require_once '_add-ons/s3files/hooks.s3files.php';
 
 ob_start();
 header('Content-Type: application/json');
-$instance = new S3StreamWrapper();
+$instance = new Hooks_s3files();
 $instance->uploadFile();
 ob_flush();
--- _add-ons/s3files/config.php
+++ _add-ons/s3files/config.php
@@ -6,4 +6,4 @@ define('S3FILES_AUTHOR', 'Chad Clark');
 define('S3FILES_AUTHOR_URL', 'http://chadjclark.com/');
 define('S3FILES_CONFIG_PATH', BASE_PATH . '/_config/add-ons/s3files');
 
-//require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'vendor/aws/aws-autoloader.php';
