1744dbf7600afb739793f8c6153e9a7d2bfc7588
Chad Clark <chad@Chads-iMac.local>
2014-04-20 12:50:55 -0400

    Grunt compress is working like a boss. Delete `node_modules`, run `npm install` and then run `grunt compress`
    Signed-off-by: Chad Clark <chad@Chads-iMac.local>

--- grunt/compress.js
+++ grunt/compress.js
@@ -1,25 +1,34 @@
 module.exports = {
 
 	compress: {
-		main: {
-			options: {
-				archive: 'fileclerk.zip',
-				mode: 'zip',
-				level: 1,
-				pretty: true
-			},
-			files: [
-				{src: ['_add-ons/fileclerk/assets/build/img/'], dest: 'fileclerk/assets/build/img/'}, // Optimized images
-				{src: ['_add-ons/fileclerk/css/fileclerk.min.css'], dest: 'fileclerk/css/', filter: 'isFile'}, // Minified CSS
-				{src: ['_add-ons/fileclerk/js/build/'], dest: 'fileclerk/js/build/', filter: 'isFile'}, // Minified JS
-				{src: ['_add-ons/fileclerk/views/'], dest: 'fileclerk/views/', filter: 'isFile'}, // Fieldtype Views
-				{src: ['_add-ons/fileclerk/config.php'], dest: '_add-ons/fileclerk/', filter: 'isFile'}, // Config File
-				{src: ['_add-ons/fileclerk/ft.fileclerk.php'], dest: '_add-ons/fileclerk/', filter: 'isFile'}, // Fieldtype File
-				{src: ['_add-ons/fileclerk/hooks.fileclerk.php'], dest: '_add-ons/fileclerk/', filter: 'isFile'}, // Hooks File
-				{src: ['_add-ons/fileclerk/pi.fileclerk.php'], dest: '_add-ons/fileclerk/', filter: 'isFile'}, // Plugin File
-				{src: ['_add-ons/fileclerk/tasks.fileclerk.php'], dest: '_add-ons/fileclerk/', filter: 'isFile'} // Tasks File
-			]
-		}
+		options: {
+			archive: 'fileclerk.zip',
+			mode: 'zip',
+			level: 1,
+			pretty: true
+		},
+		files: [
+			{src: ['_add-ons/fileclerk/assets/build/img/**.*']}, // Optimized images
+			{src: ['_add-ons/fileclerk/css/fileclerk.min.css'], filter: 'isFile'}, // Minified CSS
+			{src: ['_add-ons/fileclerk/js/build/**.js'], filter: 'isFile'}, // Minified JS
+			{src: ['_add-ons/fileclerk/views/'], filter: 'isFile'}, // Fieldtype Views
+			{src: ['_add-ons/fileclerk/config.php'], filter: 'isFile'}, // Config File
+			{src: ['_add-ons/fileclerk/ft.fileclerk.php'], filter: 'isFile'}, // Fieldtype File
+			{src: ['_add-ons/fileclerk/hooks.fileclerk.php'], filter: 'isFile'}, // Hooks File
+			{src: ['_add-ons/fileclerk/pi.fileclerk.php'], filter: 'isFile'}, // Plugin File
+			{src: ['_add-ons/fileclerk/tasks.fileclerk.php'], filter: 'isFile'}, // Tasks File
+
+			{src: ['_add-ons/fileclerk/vendor/autoload.php'], filter: 'isFile'}, // Autoload file
+			{src: ['_add-ons/fileclerk/vendor/composer/**/*']}, // Composer Files
+			{src: ['_add-ons/fileclerk/vendor/guzzle/**/*']}, // Guzzle
+			{src: ['_add-ons/fileclerk/vendor/symfony/**/*']}, // Symfony Finder, etc.
+			{src: ['_add-ons/fileclerk/vendor/aws/aws-sdk-php/build/**/*']}, // AWS SDK Build Folder
+			{src: ['_add-ons/fileclerk/vendor/aws/aws-sdk-php/src/Aws/Common/**/*']}, // AWS SDK S3 Library
+			{src: ['_add-ons/fileclerk/vendor/aws/aws-sdk-php/src/Aws/S3/**/*']}, // AWS SDK S3 Library
+
+			{src: ['_config/add-ons/fileclerk/fileclerk.yaml'], filter: 'isFile'}, // File Clerk main config
+			{src: ['_config/add-ons/fileclerk/destinations/**.*']} // File Clerk Destination Example(s)
+		]
 	}
 
 };
