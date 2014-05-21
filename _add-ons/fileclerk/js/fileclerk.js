980826ab521d71ea6726639f2b77df62ce3d593a
Chad Clark <chad@chadjclark.com>
2014-05-21 13:59:02 -0400

    JS now checks for `true` or `1` for `is_image`; Updated `ft.fileclerk.html` to check if `is_image` is `true` or `false`
    Signed-off-by: Chad Clark <chad@chadjclark.com>

--- _add-ons/fileclerk/js/fileclerk.js
+++ _add-ons/fileclerk/js/fileclerk.js
@@ -207,7 +207,7 @@ $(function () {
 								uploadPreview.attr('href', data.data.fullpath); // Update URL
 								uploadPreviewImg.attr('src', ''); // Empty img src tag if content already exists
 
-								if( hiddenIsImage.val() === 'true' )
+								if( hiddenIsImage.val() === 'true' || '1' )
 								{
 									uploadPreview.unbind();
 									uploadPreview.attr('rel', 'inline'); // rel='inline' if mime type is image
@@ -562,7 +562,7 @@ $(function () {
 			uploadPreview.attr('href', fullPath); // Update URL
 			uploadPreviewImg.attr('src', ''); // Empty img src tag if content already exists
 
-			if( hiddenIsImage.val() === 'true' )
+			if( hiddenIsImage.val() === 'true' || '1' )
 			{
 				uploadPreview.unbind();
 				uploadPreview.attr('rel', 'inline'); // rel='inline' if mime type is image
